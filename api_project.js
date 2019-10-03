4d4b6e9af0313408a96be0662b0b1e2715c09e80
BejoJeffrin23 <bejojeffrin23@gmail.com>
2019-10-03 09:30:46 +0530

    apidoc

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Scheduler application",
+  "version": "0.0.1",
+  "description": "Documentation for the scheduler application",
+  "title": "Scheduler APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-02T04:19:19.391Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
